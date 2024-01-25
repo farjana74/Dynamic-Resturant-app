2cbd89f3db141382d7fe48cd503e79fff7ced281
farjana <farjanaCse74@gmail.com>
2024-01-25 15:19:50 -0800

    added dynamic banner button

--- src/Page/Home/Banner/Banner.js
+++ src/Page/Home/Banner/Banner.js
@@ -4,19 +4,24 @@ import banner1 from "../../../images/baner.png";
 import banner2 from "../../../images/baner1.png";
 import banner3 from "../../../images/first-slider.png";
 import Carousel from "react-bootstrap/Carousel";
+import Navigation from "../../Shared/Navigation/Navigation";
 const Banner = () => {
   return (
     <div className="banner-bg">
+        <Navigation/>
       <Carousel data-bs-theme="dark">
         <Carousel.Item>
         <div className="container">
             <div className="row banner-bg-1 d-flex justify-content-between align-items-center">
               <div className="col-md-6">
-                <p>Best In Cafe</p>
-                <h2>Different Spice For Different Taste</h2>
+              <p className="banner-header">Best In Cafe</p>
+                <h2 className="banner-text">Different <span className="text-spice">Spice</span> For Different Taste</h2>
+                <div className="pt-5">
+                <button className="order-btn">Order Now</button>
+                </div>
               </div>
               <div className="col-md-6">
-              <img className="d-block w-100 h-50" src={banner1} alt="First slide" />
+              <img className=" hero-img d-block w-100 " src={banner1} alt="First slide" />
               </div>
             </div>
           </div>
@@ -26,12 +31,15 @@ const Banner = () => {
           <div className="container">
             <div className="row banner-bg-1 d-flex justify-content-between align-items-center">
               <div className="col-md-6">
-                <p>Best In Cafe</p>
-                <h2>Different Spice For Different Taste</h2>
+                <p className="banner-header">Best In Cafe</p>
+                <h2 className="banner-text">Different <span className="text-spice">Spice</span> For Different Taste</h2>
+                <div className="pt-5">
+                <button className="order-btn">Order Now</button>
+                </div>
               </div>
               <div className="col-md-6">
                 <img
-                  className="d-block w-100 h-50"
+                  className=" hero-img d-block w-100 "
                   src={banner2}
                   alt="Second slide"
                 />
@@ -43,11 +51,14 @@ const Banner = () => {
           <div className="container">
             <div className="row banner-bg-1 d-flex justify-content-between align-items-center">
               <div className="col-md-6">
-                <p>Best In Cafe</p>
-                <h2>Different Spice For Different Taste</h2>
+              <p className="banner-header">Best In Cafe</p>
+                <h2 className="banner-text">Different <span className="text-spice">Spice</span> For Different Taste</h2>
+                <div className="pt-5">
+                <button className="order-btn">Order Now</button>
+                </div>
               </div>
               <div className="col-md-6">
-                <img className=" w-100 " src={banner3} alt="Third slide" />
+                <img className=" w-100 hero-img " src={banner3} alt="Third slide" />
               </div>
             </div>
           </div>
